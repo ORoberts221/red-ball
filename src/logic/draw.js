bab7044a1d19040cbc5335786521211d456ed277
Owen <Owen.Roberts@stormid.com>
2019-05-07 15:38:27 +0100

    Added some colours

--- src/logic/draw.js
+++ src/logic/draw.js
@@ -4,6 +4,8 @@ export default function draw() {
     var c = document.getElementById("canvas");
     var ctx = c.getContext("2d");
 
+    ctx.fillStyle = "white";
+
     // module aliases
     var Engine = Matter.Engine,
         Render = Matter.Render,
@@ -14,13 +16,25 @@ export default function draw() {
     var engine = Engine.create();
 
     // create a circle
-    var ball = Bodies.circle(c.width / 2, c.height / 2, 25);
+    var ball = Bodies.circle(c.width / 2, c.height / 2, 25, {
+        render :{
+            fillStyle: 'red',
+        }
+    });
 
     //create the ground
-    var ground = Bodies.rectangle(0, 450, 1000, 0.5, { isStatic: true });
+    var ground = Bodies.rectangle(0, 495, 1000, 10, { 
+        isStatic: true,
+        render: {
+            fillStyle: '#222'
+        } 
+    });
 
     // add the bodies to the world
-    World.add(engine.world, [ball, ground]);
+    World.add(engine.world, [
+        ball, 
+        ground
+    ]);
 
     // run the engine
     Engine.run(engine);
@@ -31,9 +45,13 @@ export default function draw() {
         options: {
             width: c.width,
             height: c.height,
+            wireframes: false,
+            background: '#ffffff'
         },
         engine: engine
     });
+
+    ctx.fillStyle = 'rgba(0,0,0,0.5)';
 
     // run the renderer
     Render.run(render);
