846780dc814b6628e76d8126ab8871b14ebd4bb3
Owen <Owen.Roberts@stormid.com>
2019-05-07 16:41:57 +0100

    removed canvas component

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -1,16 +1,20 @@
 import React, { Component } from 'react';
-import Canvas from "../canvas";
+import draw from "../../logic/draw";
 import './index.css';
 
 
 class App extends Component {
+  componentDidMount() {
+    draw();
+  }
+
   render() {
     return (
       <div className="container">
         <div>
           <h2>Red Ball</h2>
         </div>
-        <Canvas />
+        <canvas id="canvas" className="canvas" width="500" height="500"></canvas>
       </div>
     );
   }
