f43576a9af818c25306f98650975bb3c075c6a12
Owen <Owen.Roberts@stormid.com>
2019-05-07 15:58:20 +0100

    Added a mouse contraint to allow clicking and dragging the ball

--- src/logic/draw.js
+++ src/logic/draw.js
@@ -4,43 +4,35 @@ export default function draw() {
     var c = document.getElementById("canvas");
     var ctx = c.getContext("2d");
 
-    ctx.fillStyle = "white";
-
     // module aliases
     var Engine = Matter.Engine,
         Render = Matter.Render,
         World = Matter.World,
-        Bodies = Matter.Bodies;
+        Bodies = Matter.Bodies,
+        Mouse = Matter.Mouse,
+        MouseConstraint = Matter.MouseConstraint;
 
     // create an engine
     var engine = Engine.create();
 
     // create a circle
     var ball = Bodies.circle(c.width / 2, c.height / 2, 25, {
-        render :{
+        friction: 0.0025,
+        render: {
             fillStyle: 'red',
         }
     });
 
     //create the ground
-    var ground = Bodies.rectangle(0, 495, 1000, 10, { 
+    var ground = Bodies.rectangle(0, 495, 1000, 10, {
         isStatic: true,
         render: {
             fillStyle: '#222'
-        } 
+        }
     });
 
-    // add the bodies to the world
-    World.add(engine.world, [
-        ball, 
-        ground
-    ]);
-
-    // run the engine
-    Engine.run(engine);
-
-     // create a renderer
-     var render = Render.create({
+    // create a renderer
+    var render = Render.create({
         canvas: c,
         options: {
             width: c.width,
@@ -51,7 +43,28 @@ export default function draw() {
         engine: engine
     });
 
-    ctx.fillStyle = 'rgba(0,0,0,0.5)';
+    //Add mouse constraint
+    var mouse = Mouse.create(render.canvas),
+        mouseConstraint = MouseConstraint.create(engine, {
+            mouse: mouse,
+            constraint: {
+                stiffness: 0.2,
+                render: {
+                    visible: false
+                }
+            }
+        });
+
+
+    // add the bodies to the world
+    World.add(engine.world, [
+        mouseConstraint,
+        ball,
+        ground
+    ]);
+
+    // run the engine
+    Engine.run(engine);
 
     // run the renderer
     Render.run(render);
