3dea88b75fba76a04b554d1be5a0418dea6d5878
Owen <Owen.Roberts@stormid.com>
2019-05-08 17:06:01 +0100

    Canvas will now expand to fill available screen space on startup

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -13,11 +13,18 @@ class App extends Component {
     }
   }
 
+  setupCanvas(canvas){
+    canvas.width = document.body.clientWidth;
+    canvas.height = document.body.clientHeight;
+  }
+
   setupWorld(canvas) {
     //create ball
+    console.log("creating ball at");
+
     var ball = new Ball(canvas.width / 2, canvas.height / 2);
     //create ground
-    var ground = Matter.Bodies.rectangle(0, 495, canvas.width * 2, 20, { isStatic: true, render: { fillStyle: '#222' } });
+    var ground = Matter.Bodies.rectangle(0, canvas.height, canvas.width * 2, 20, { isStatic: true, render: { fillStyle: '#222' } });
     //create drag constraint
     var dragConstraint = Drag(canvas, this.state.engine);
 
@@ -31,6 +38,8 @@ class App extends Component {
 
   componentDidMount() {
     var canvas = document.getElementById("canvas");
+    this.setupCanvas(canvas);
+
     this.setupWorld(canvas);
     var renderer = createRender(canvas, this.state.engine);
     this.run(renderer);
@@ -40,7 +49,7 @@ class App extends Component {
   render() {
     return (
       <div className="container">
-        <div>
+        <div className="header">
           <h2>Red Ball</h2>
           <p>A small experiment with React and <a href="http://brm.io/matter-js/">Matter.js</a></p>
         </div>
